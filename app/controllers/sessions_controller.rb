313c2ec80d753ece7b4e79d48a99058026adf085
Hillevi Eklöw <hillevieklow@gmail.com>
2018-08-09 18:07:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # if params[:name].present?
-    #   session[:name] = params[:name]
-    # end
-    # redirect_to '/'
-    # NOTE: need to use if block or else get double render error!
     if !params[:name].present?
       redirect_to login_path
     else
